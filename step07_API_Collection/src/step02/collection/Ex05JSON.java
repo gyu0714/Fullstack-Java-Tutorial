f5aa5b35e099027e81b11d4f761a5d33954ed227
gyu0714 <lagyu99@gmail.com>
2022-06-13 11:10:02 +0900

    JSON-java DONE

---
+++ step07_API_Collection/src/step02/collection/Ex05JSON.java
@@ -0,0 +1,53 @@
+package step02.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//import org.json.JSONObject;
+import org.json.*;
+
+import model.domain.Student;
+
+public class Ex05JSON {
+	public static void main(String[] args) {
+		// JSON Object
+//		JSONObject jsonObject = new JSONObject();
+		
+//		jsonObject.put("name", "Java");
+//		jsonObject.put("age", 27);
+//		System.out.println(jsonObject);
+		
+		// HashMap 으로 JSONObject 생성
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", "Java");
+		map.put("age", "27");
+		
+//		JSONObject jsonObject2 = new JSONObject(map);
+//		System.out.println(jsonObject2);
+//		
+		// JSON 문자열로 JSONObject 생성
+//		JSONObject jsonObject3 = new JSONObject("{\"name\":\"Java\", \"age\":\"27\"}");
+		
+		// POJO로 JSONObject 생성
+		Student student1 = new Student("Java", 1, "Junior");
+//		JSONObject jsonObject4 = new JSONObject(student1);
+//		System.out.println(jsonObject4.get("name"));
+		
+		// List로 JSONObject 생성
+		List<String> friendList = new ArrayList<String>();
+		friendList.add("Sam");
+		friendList.add("Harry");
+		
+		JSONArray jsonFriendList = new JSONArray(friendList);
+//		System.out.println(friendList);
+		System.out.println(jsonFriendList);
+		
+		JSONObject jsonObject5 = new JSONObject();
+//		jsonObject5.put("name", "Java");
+//		jsonObject5.put("age", "27");
+//		jsonObject5.put("friends", jsonFriendList);
+	}
+
+}
